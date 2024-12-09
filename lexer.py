27c5cccae3b9adbfcecf824f85bddd68f4f63c82
JVictor011 <jvictor2021@alu.ufc.br>
2024-12-08 23:03:18 -0300

    novos tipos de padrão

--- lexer.py
+++ lexer.py
@@ -8,14 +8,16 @@ def AnalisadorLexico(codigo):
     padrao_identificador = r'[a-zA-Z_][a-zA-Z0-9_]*'  # Identificadores podem começar com _ ou letra
     padrao_numero = r'\d+'  # Números inteiros
     padrao_operadores = r'[+\-*/=]'  # Operadores aritméticos
-    padrao_operadores_logicos = r'&&|\|\||!'  # Operadores lógicos
+    padrao_operadores_logicos = r'(\|\||&&|!)'  # Operadores lógicos (||, &&, !)
     padrao_strings = r'"[^"]*"'  # Strings entre aspas
     padrao_comentarios = r'//[^\n]*'  # Comentários de linha
-    padrao_tipo = r'\b(int|float|bool)\b'  # Tipos de dados
+    padrao_tipo = r'\b(int|float|bool|double)\b'  # Tipos de dados
     padrao_parenteses = r'[\(\)]'  # Parênteses
-    padrao_chaves = r'[\{\}]'  # Chaves
     padrao_ponto_virgula = r';'  # Ponto e vírgula
-    padrao_palavra_chave = r'\b(if|else|while|return)\b'
+    padrao_palavra_chave = r'\b(if|else|while|return|do)\b'
+    padrao_comparacao = r'(==|!=|<=|>=)'  # Comparações == e !=
+    padrao_delimitadores = r'[\{\}:]'  # Delimitadores: {, }, :
+    padrao_booleano = r'\b(true|false)\b'  # Booleanos
 
     # Remover espaços em branco
     codigo = codigo.strip()
@@ -27,17 +29,19 @@ def AnalisadorLexico(codigo):
 
         # Testa cada padrão
         for padrao, tipo in [
-            (padrao_comentarios, None),
-            (padrao_identificador, 'IDENTIFICADOR'),
+            (padrao_comentarios, 'COMENTARIOS'),
             (padrao_numero, 'NUMERO'),
+            (padrao_comparacao, 'COMPARACAO'),
             (padrao_operadores, 'OPERADOR'),
             (padrao_operadores_logicos, 'OPERADOR_LOGICO'),
             (padrao_strings, 'STRING'),
             (padrao_tipo, 'TIPO'),
             (padrao_parenteses, 'PARENTESES'),
-            (padrao_chaves, 'CHAVE'),
             (padrao_ponto_virgula, 'PONTO_VIRGULA'),
-            (padrao_palavra_chave, 'PALAVRA_CHAVE')
+            (padrao_palavra_chave, 'PALAVRA_CHAVE'),
+            (padrao_booleano, 'BOOLEANO'),
+            (padrao_identificador, 'IDENTIFICADOR'),
+            (padrao_delimitadores, 'DELIMITADOR')
         ]:
             match = re.match(padrao, codigo)
             if match:
