9ab3a7af952e038f1792bae052c8ec2e8f986783
AlbertoMarinho <albertomarinho51@gmail.com>
2024-12-11 23:11:38 -0300

    feat: adição de mais casos no teste de sentença.

--- tester.py
+++ tester.py
@@ -3,10 +3,10 @@ from lark import Lark, UnexpectedInput
 grammar = r"""
     start: program
     program: command*
-    command: assignment | ifstmt | while_stmt | do_while_stmt | return_stmt
+    command: assignment | if_stmt | while_stmt | do_while_stmt | return_stmt
     assignment: identifier "=" expression ";"
     expression: term (("+"|"-") term)*
-    term: factor ((""|"/") factor)
+    term: factor (("*"|"/") factor)*
     factor: identifier | number | "true" | "false" | "(" expression ")" | string
     if_stmt: "if" expression block else_block?
     else_block: "else" block
@@ -14,23 +14,32 @@ grammar = r"""
     while_stmt: "while" expression block
     do_while_stmt: "do" block "while" expression ";"
     return_stmt: "return" expression ";"
-    identifier: /[a-zA-Z][a-zA-Z0-9_]/
+    identifier: /[a-zA-Z_][a-zA-Z0-9_]*/
     number: /\d+/
-    string: /"[^"]"/
-    %ignore ///[^\n]/
+    string: /"[^"]*"/
+    %ignore /\/\/[^\n]*/
     %ignore /[ \t\r\n]+/  // Ignora espaços em branco e novas linhas
 """
+
+
 
 
 
 parser = Lark(grammar, start="start", parser="lalr")
 
 test_sentences = [
-    "a = b + c;",
-    "if true { a = 1; } else { b = 2; }",
-    "while x { y = y 2; }",
-    "do { x = x - 1; } while x;",
-    "return a + b * c;"
+    "a = b + c;",  # Válida
+    "if true { a = 1; } else { b = 2; }",  # Válida
+    "while x { y = y * 2; }",  # Válida
+    "do { x = x - 1; } while x;",  # Válida
+    "return a + b * c;",  # Válida
+    "a = 42 + ;",  # Inválida: operador sem operando à direita
+    "if (x == 10) { y = y + 1; } else ;",  # Inválida: bloco else vazio
+    "x = 10 + * 2;",  # Inválida: dois operadores consecutivos sem operando
+    "while { x = x / 2; }",  # Inválida: condição de loop while ausente
+    "do { x = x - 1; while x;",  # Inválida: falta de fechamento do bloco do while
+    "return ;",  # Inválida: falta de expressão após return
+    "if true { a = 1 }"  # Inválida: falta de ponto e vírgula após o comando
 ]
 
 
@@ -38,6 +47,6 @@ for sentence in test_sentences:
     try:
         print(f"Testando: {sentence}")
         tree = parser.parse(sentence)
-        print("Valido!")
+        print("Valido!\n")
     except UnexpectedInput as e:
         print(f"Erro: {e}")
